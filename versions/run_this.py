327f92b3ea666b66412600a3c16dfbad4c21aebd
Nikolas Schnellbächer <khx0@posteo.net>
2018-07-17 10:10:39 +0200

    added run_this.py script

---
+++ versions/run_this.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# author: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2018-07-17
+# file: run_this.py
+##########################################################################################
+
+import sys
+import os
+import subprocess
+import time
+import datetime
+
+def ensure_dir(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+now = datetime.datetime.now()
+now = "%s-%s-%s" %(now.year, str(now.month).zfill(2), str(now.day).zfill(2))
+
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+RAWDIR = os.path.join(BASEDIR, 'raw')
+OUTDIR = os.path.join(BASEDIR, 'out')
+
+# useful meta properties of any python script
+print __file__
+print os.path.abspath(__file__)
+print os.path.dirname(os.path.abspath(__file__))
+
+# print globals()
+
+
+
+
+
