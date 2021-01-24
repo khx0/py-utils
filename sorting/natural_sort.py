183313364a1d98581118f8e57fe9ac2f142b1fb8
Nikolas Schnellbächer <khx0@posteo.net>
2021-01-24 13:07:11 +0100

    added natural_sort module

---
+++ sorting/natural_sort.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+##########################################################################################
+# adopted by: Nikolas Schnellbaecher
+# contact: khx0@posteo.net
+# date: 2021-03-24
+# file: natural_sort.py
+##########################################################################################
+
+import re
+
+def natural_keys(key):
+    """
+    Natural keys for natural order list sorting
+    :param key: Input key for sorting criteria.
+    :return: Natural (ordered) key.
+    Usage:
+    a_list.sort(key=natural_keys), where a_list is a given (potentially) unsorted list
+    """
+    unify = lambda x: int(x) if x.isdigit() else x.lower()
+    return [unify(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', key)]
+
+def natural_sort(l):
+    """
+    Natural sorting algorithm. Sorts a given list l in human order.
+    :param l: (Potentially) Unsorted input list.
+    :return:  Naturally sorted list.
+    """
+    unify = lambda x: int(x) if x.isdigit() else x.lower()
+    natural_keys = lambda key: [unify(c) for c in re.split(r'[+-]?([0-9]+(?:[.][0-9]*)?|[.][0-9]+)', key)]
+    return sorted(l, key=natural_keys)
+
+if __name__ == '__main__':
+
+    pass
+
+    # describe the two main usage modes here
+    # use like this
+    # alist.sort(key = natural_keys)
